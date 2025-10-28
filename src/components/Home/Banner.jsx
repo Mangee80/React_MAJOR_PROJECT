f90ec7967c0766708479c06a9dee83e2909c4f5b
Mani Pratap Singh <sDemanipratap@gmail.com>
2025-10-28 23:52:11 +0530

    feat: Add typewriter effect to homepage banner

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,11 +1,13 @@
 import styles from "./Banner.module.css";
 import Carousel from "./Carousel";
+import useTypewriter from "../../hooks/useTypewriter";
 const Banner = () => {
+  const typedText = useTypewriter("Unlock a World of Entertainment.", 100);
   return (
     <div className={styles.bannerContainer}>
       <Carousel />
       <div className={styles.bottom}>
-        <p>Unlock a World of Entertainment.</p>
+        <p>{typedText}</p>
       </div>
     </div>
   );
